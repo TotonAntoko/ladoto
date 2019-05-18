d525df7d369096e979d0bebdb9c3ccf56f7b4d25
Tdau123 <Totonantoko@gmail.com>
2019-05-19 06:13:10 +0700

    Menambah Template FrontEnd

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,64 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/**
+ * Customer routes
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontEnd.index');
 });
 
-Route::get('/admin/', function () {
-    return view ('admin.index');
+// Route::get('/login', function () {
+//     return view('frontEnd.login');
+// });
+
+// Route::get('/reg', function () {
+//     return view('frontEnd.register');
+// });
+
+Route::get('/product', function () {
+    return view('frontEnd.product');
 });
 
-// CUSTOMER
-Route::get('/admin/customer/', 'CustomerController@index');
-Route::post('/admin/customer/create', 'CustomerController@create');
-Route::put('/admin/customer/update', 'CustomerController@update');
-Route::delete('/admin/customer/delete', 'CustomerController@destroy');
-
-// INVENTORY
-Route::get('/admin/inventory/', 'InventoryController@index');
-Route::post('/admin/inventory/create', 'InventoryController@create');
-Route::put('/admin/inventory/update', 'InventoryController@update');
-Route::delete('/admin/inventory/delete', 'InventoryController@destroy');
-
-Route::get('/admin/brand', function () {
-    return view ('/admin/brand');
+Route::get('/details', function () {
+    return view('frontEnd.details');
 });
+
+Route::get('/contact', function () {
+    return view('frontEnd.contact');
+});
+
+
+/**
+ * Admin routes
+ */
+// Route::namespace('Admin')->group(function () {
+//     Route::get('admin/login', 'LoginController@showLoginForm')->name('admin.login');
+//     Route::post('admin/login', 'LoginController@login')->name('admin.login');
+//     Route::get('admin/logout', 'LoginController@logout')->name('admin.logout');
+// });
+// Route::group(['prefix' => 'admin', 'middleware' => ['employee'], 'as' => 'admin.' ], function () {
+//     Route::namespace('Admin')->group(function () {
+//         Route::get('/', 'DashboardController@index')->name('dashboard');
+//     });
+// });
+
+// Route::get('/admin/', function () {
+//     return view ('admin.index');
+// });
+
+// // CUSTOMER
+// Route::get('/admin/customer/', 'CustomerController@index');
+// Route::post('/admin/customer/create', 'CustomerController@create');
+// Route::put('/admin/customer/update', 'CustomerController@update');
+// Route::delete('/admin/customer/delete', 'CustomerController@destroy');
+
+// // INVENTORY
+// Route::get('/admin/inventory/', 'InventoryController@index');
+// Route::post('/admin/inventory/create', 'InventoryController@create');
+// Route::put('/admin/inventory/update', 'InventoryController@update');
+// Route::delete('/admin/inventory/delete', 'InventoryController@destroy');
+
+// Route::get('/admin/brand', function () {
+//     return view ('/admin/brand');
+// });
